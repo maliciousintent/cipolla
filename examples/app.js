4c9b29e214b754eb3f53cc86dcd9852558e15368
Simone Lusenti <simone@plasticpanda.com>
2013-11-14 12:34:04 +0100

    logging works again: updated to coolog@3, exporting coolog module, updated examples

--- examples/app.js
+++ examples/app.js
@@ -1,5 +1,19 @@
+/*jshint node:true, eqnull:true, laxcomma:true, undef:true, indent:2, camelcase:false */
+'use strict';
 
-var cipolla = require('../');
+var cipolla = require('../')
+  , logentries = require('coolog-appender-logentries')(process.env.LOGENTRIES_APIKEY)
+  , coolog = require('coolog')
+  , logger
+  ;
+
+cipolla.coolog.addChannel({ name: 'cipolla', level: 'debug', appenders: ['console', logentries] });
+
+coolog.addChannel({ name: 'root', level: 'debug', appenders: ['console', logentries] });
+logger = coolog.logger('app.js', 'root');
+
+logger.log('fooo');
+logger.log('apikey', process.env.LOGENTRIES_APIKEY);
 
 
 if (process.env.NODE_ENV === 'production' && !process.env.CIPOLLA_FOREVER) {
@@ -8,13 +22,11 @@ if (process.env.NODE_ENV === 'production' && !process.env.CIPOLLA_FOREVER) {
   
 } else {
   
-  
   var router = new cipolla.Dispatcher(__dirname);
   
   cipolla({
     name: 'allenatori',
     port: process.env.PORT || 3000,
-    logentriesAPIKey: process.env.LOGENTIES_APIKEY,
     cwd: __dirname,
     dispatcher: router
   });
